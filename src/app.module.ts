dd350ef5bc629d1c5d547fcbb552d68199a4c20f
omerhabib62 <omer.habib62@gmail.com>
2025-07-20 16:34:00 +0500

    feat: add CommonModule to the application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ProjectsModule } from './projects/projects.module';
 import { BidsModule } from './bids/bids.module';
 import { LearningModule } from './learning/learning.module';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [UsersModule, ProjectsModule, BidsModule, LearningModule, AuthModule],
+  imports: [UsersModule, ProjectsModule, BidsModule, LearningModule, AuthModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
